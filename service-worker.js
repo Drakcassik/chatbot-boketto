c297da786a1db57d25c1514ce87761662bd1dbf6
Drakcassik <81758528+Drakcassik@users.noreply.github.com>
2025-10-27 19:09:08 -0300

    Simplify event listener code in service worker

--- service-worker.js
+++ service-worker.js
@@ -9,15 +9,11 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
 
 self.addEventListener('activate', e => {
